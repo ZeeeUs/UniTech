ab065ac4d1b697694d842603cb9b4b17906b1ef1
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-01-13 17:17:39 +0300

    Show data from Postgres

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from .models import Technik, Territory, OrderDuration, Order
 
+
 # Create your views here.
 
 def index(request):
-    num_technik=Technik.objects.all().count()
-    num_Territory=Territory.objects.all().count()
-    num_orderduration=OrderDuration.objects.all().count()
+    # num_technik=Technik.objects.all().count()
+    # num_Territory=Territory.objects.all().count()
+    # num_orderduration=OrderDuration.objects.all().count()
+    #
+    # return render(
+    #     request,
+    #     'index.html',
+    #     context={'num_technik':num_technik, 'num_Territory':num_Territory, 'num_orderduration':num_orderduration},
+    # )
+    technik = Technik.objects.all()
 
-    return render(
-        request,
-        'index.html',
-        context={'num_technik':num_technik, 'num_Territory':num_Territory, 'num_orderduration':num_orderduration},
-    )
+    context = {
+        'tch' : technik
+    }
+
+    return render(request, 'index.html', context)
