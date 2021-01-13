7cb980ee5907329817e3fffe1e0542aaa7937a58
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-01-13 14:09:59 +0300

    Add pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Technik, Territory, OrderDuration, Order
 
 # Create your views here.
+
+def index(request):
+    num_technik=Technik.objects.all().count()
+    num_Territory=Territory.objects.all().count()
+    num_orderduration=OrderDuration.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_technik':num_technik, 'num_Territory':num_Territory, 'num_orderduration':num_orderduration},
+    )
