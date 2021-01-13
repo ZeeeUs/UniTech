789b08d5bca1932158f4745c51ff43a03d5f814a
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-01-13 04:13:19 +0300

    Create and register django models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Technik, Territory, OrderDuration, Order
+
 # Register your models here.
+admin.site.register(Technik)
+admin.site.register(Territory)
+admin.site.register(OrderDuration)
+admin.site.register(Order)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+import uuid
+
+
+class Technik(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Territory(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class OrderDuration(models.Model):
+    worktime = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.worktime
+
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    technik = models.ForeignKey('Technik', on_delete=models.CASCADE)
+    customer = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=12)
+    email = models.CharField(max_length=255)
+    territory = models.ForeignKey('Territory', on_delete=models.SET_NULL, null=True)
+    duration = models.ForeignKey('OrderDuration', on_delete=models.PROTECT)
+    note = models.CharField(max_length=255)
+    datee = models.DateField
+
+
+    def __str__(self):
+        return
+
+    def get_absolute_url(self):
+        return reverse('order-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return '{0}.{1}'.format(self.id, self.technik.name)
