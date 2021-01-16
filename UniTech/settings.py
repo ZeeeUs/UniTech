250f6eb0dec95483a993798e34b1aeca53005770
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-01-17 00:09:08 +0300

    Change Database config

--- UniTech/settings.py
+++ UniTech/settings.py
@@ -76,14 +76,21 @@ WSGI_APPLICATION = 'UniTech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'unitech',
+#         'USER': 'zeus',
+#         'PASSWORD': '085001',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'unitech',
-        'USER': 'zeus',
-        'PASSWORD': '085001',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
