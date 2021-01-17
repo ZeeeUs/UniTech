1a132b1aa96bd4e7cc269ccaf18404beaf8dfd8f
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-01-17 18:46:33 +0300

    Возобновление работы

--- UniTech/settings.py
+++ UniTech/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'UniTech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'unitech',
-#         'USER': 'zeus',
-#         'PASSWORD': '085001',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unitech',
+        'USER': 'zeus',
+        'PASSWORD': '085001',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
